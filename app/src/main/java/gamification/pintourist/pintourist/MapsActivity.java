dee07ad5a8de94e7252aa9b6f1262490c7d69b11
Marco Favorito <marco.favorito@gmail.com>
2015-06-21 19:43:33 +0100

    Implementazione del menu laterale N.B. Ho praticamente copiato alcuni dei file della repository a questo url: https://github.com/gabrielemariotti/androiddev/tree/master/NavigationDrawer

--- app/src/main/java/gamification/pintourist/pintourist/MapsActivity.java
+++ app/src/main/java/gamification/pintourist/pintourist/MapsActivity.java
@@ -1,22 +1,31 @@
 package gamification.pintourist.pintourist;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.graphics.drawable.AnimationDrawable;
 import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -49,9 +58,10 @@ public class MapsActivity extends ActionBarActivity {
 
 
 
-    private String[] mPlanetTitles;
-    private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
+    private DrawerLayout mDrawer;
+    private CustomActionBarDrawerToggle mDrawerToggle;
+    private String[] menuItems;
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +72,21 @@ public class MapsActivity extends ActionBarActivity {
         //requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         setContentView(R.layout.activity_map);
+
+
+        //getActionBar().setDisplayHomeAsUpEnabled(true);
+        //getActionBar().setHomeButtonEnabled(true);
+
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        // set a custom shadow that overlays the main content when the drawer
+        // opens
+        mDrawer.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+
+        _initMenu();
+        mDrawerToggle = new CustomActionBarDrawerToggle(this, mDrawer);
+        mDrawer.setDrawerListener(mDrawerToggle);
+
 
         /*
 
@@ -95,6 +120,132 @@ public class MapsActivity extends ActionBarActivity {
         startGame();
     }
 
+    private void _initMenu() {
+        NsMenuAdapter mAdapter = new NsMenuAdapter(this);
+
+        // Add Header
+        mAdapter.addHeader(R.string.ns_menu_main_header);
+
+        // Add first block
+
+        menuItems = getResources().getStringArray(
+                R.array.ns_menu_items);
+        String[] menuItemsIcon = getResources().getStringArray(
+                R.array.ns_menu_items_icon);
+
+        int res = 0;
+        for (String item : menuItems) {
+
+            int id_title = getResources().getIdentifier(item, "string",
+                    this.getPackageName());
+            int id_icon = getResources().getIdentifier(menuItemsIcon[res],
+                    "drawable", this.getPackageName());
+
+            NsMenuItemModel mItem = new NsMenuItemModel(id_title, id_icon);
+            if (res==1) mItem.counter=12; //it is just an example...
+            if (res==3) mItem.counter=3; //it is just an example...
+            mAdapter.addItem(mItem);
+            res++;
+        }
+
+        mAdapter.addHeader(R.string.ns_menu_main_header2);
+
+        this.mDrawerList = (ListView) findViewById(R.id.drawer);
+        if (this.mDrawerList != null)
+            this.mDrawerList.setAdapter(mAdapter);
+
+        this.mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    /* Called whenever we call invalidateOptionsMenu() */
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // If the nav drawer is open, hide action items related to the content view
+        boolean drawerOpen = mDrawer.isDrawerOpen(mDrawerList);
+        menu.findItem(R.id.action_save).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+		/*
+		 * The action bar home/up should open or close the drawer.
+		 * ActionBarDrawerToggle will take care of this.
+		 */
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        // Handle your other action bar items...
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class CustomActionBarDrawerToggle extends ActionBarDrawerToggle {
+
+        public CustomActionBarDrawerToggle(Activity mActivity,DrawerLayout mDrawerLayout){
+
+            super(
+                    mActivity,
+                    mDrawerLayout,
+                    R.string.ns_menu_open,
+                    R.string.ns_menu_close);
+            //                    R.drawable.ic_drawer,
+
+        }
+
+        @Override
+        public void onDrawerClosed(View view) {
+            //getActionBar().setTitle(getString(R.string.ns_menu_close));
+            invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+        }
+
+        @Override
+        public void onDrawerOpened(View drawerView) {
+            //getActionBar().setTitle(getString(R.string.ns_menu_open)); Non funziona? problemi con l'interazione con le risorse?
+            //getActionBar().setTitle("Navigation Drawer Open Menu"); //aggiunta manualmente da me
+
+            invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+        }
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position,
+                                long id) {
+            // Highlight the selected item, update the title, and close the drawer
+            // update selected item and title, then close the drawer
+            mDrawerList.setItemChecked(position, true);
+            String text= "menu click... should be implemented";
+            Toast.makeText(MapsActivity.this, text, Toast.LENGTH_LONG).show();
+            //You should reset item counter
+            mDrawer.closeDrawer(mDrawerList);
+
+        }
+
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -102,6 +253,7 @@ public class MapsActivity extends ActionBarActivity {
         //setUpMapIfNeeded();
         mMapViewer.setUpMapIfNeeded();
     }
+
 
 
     public static Context getAppContext(){
@@ -121,8 +273,6 @@ public class MapsActivity extends ActionBarActivity {
     public void startGame(){
         TextView Suggeritore=(TextView) findViewById(R.id.suggeritore);
         Suggeritore.setText(R.string.scegliPinPartenza);
-
-
     }
 
 }
